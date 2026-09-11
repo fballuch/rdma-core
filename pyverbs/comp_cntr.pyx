7a7785ade11ecf8c03ac711b6407457cac5fabe7
Filip Balluch <fballuch@redhat.com>
2026-09-11 10:55:09 +0200

    pyverbs: Fix uint64_t type mismatch in CompCntr
    The read() and read_err() methods in CompCntr used 'unsigned long' for
    the value parameter, which is 32-bit on i686 but the C API
    (ibv_read_comp_cntr / ibv_read_err_comp_cntr) expects uint64_t.
    This caused a build failure on 32-bit architectures.
    
    Use uint64_t consistently and add the missing cimport.
    
    Signed-off-by: Filip Balluch <fballuch@redhat.com>

--- pyverbs/comp_cntr.pyx
+++ pyverbs/comp_cntr.pyx
@@ -3,6 +3,7 @@
 
 #cython: language_level=3
 
+from libc.stdint cimport uint64_t
 from pyverbs.base import PyverbsRDMAErrno, PyverbsRDMAError
 from pyverbs.device cimport Context
 cimport pyverbs.libibverbs as v
@@ -81,7 +82,7 @@ cdef class CompCntr(PyverbsCM):
 
     def read(self):
         """Read and return the current completion count."""
-        cdef unsigned long value = 0
+        cdef uint64_t value = 0
         rc = v.ibv_read_comp_cntr(self.comp_cntr, &value)
         if rc:
             raise PyverbsRDMAError('Failed to read comp_cntr', rc)
@@ -89,7 +90,7 @@ cdef class CompCntr(PyverbsCM):
 
     def read_err(self):
         """Read and return the current error count."""
-        cdef unsigned long value = 0
+        cdef uint64_t value = 0
         rc = v.ibv_read_err_comp_cntr(self.comp_cntr, &value)
         if rc:
             raise PyverbsRDMAError('Failed to read_err comp_cntr', rc)
